70e7535fae702b3f33af6fcdc6dc76647d8462ad
Rashad Hamid <mr_rashad@email.com>
2023-10-23 22:29:42 +0400

    add

--- src/main/java/com/company/Louncher.java
+++ src/main/java/com/company/Louncher.java
@@ -1,12 +1,30 @@
 package com.company;
 
 
+import com.company.config.WebConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-public class Louncher {
 
-	public static void main(String[] args) {
-		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-	}
+public class Louncher implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context;
+        context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+        context.setServletContext(servletContext);
+
+        //servletContext.addListener(new ContextLoaderListener(context));
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic servlet;
+        servlet = servletContext.addServlet("SpringWebMVC", dispatcherServlet);
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }
 
 }
