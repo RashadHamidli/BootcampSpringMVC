d0eec621ba37fbb2893be496a4f2fae114e61c14
Rashad Hamid <mr_rashad@email.com>
2023-10-23 18:08:05 +0400

    add

---
+++ src/main/java/com/companyy/bootcampspringmvc/BootcampSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.companyy.bootcampspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/companyy/bootcampspringmvc/BootcampSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.companyy.bootcampspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
