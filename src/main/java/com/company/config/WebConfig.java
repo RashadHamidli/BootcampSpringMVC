72aa2654bf055016d9aa2841f228f812f58344a0
Rashad Hamid <mr_rashad@email.com>
2023-10-26 16:38:39 +0400

    add

---
+++ src/main/java/com/company/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.company.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@ComponentScan
+@EnableWebMvc
+@ComponentScan(basePackages = "com.company")
+public class WebConfig {
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
--- src/main/java/com/company/controller/MessageController.java
+++ src/main/java/com/company/controller/MessageController.java
@@ -5,15 +5,16 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @RestController
 @RequestMapping("/app")
 public class MessageController {
     @GetMapping("/message")
     public String index(ModelMap modelMap) {
-        Message mesaj = new Message();
-        mesaj.setMessage("Merhaba Spring MVC!");
-//        modelMap.addAttribute("mesaj", mesaj.getMessage());
-        return mesaj.getMessage();
+        Message message = new Message();
+        message.setMessage("Merhaba Spring MVC! test");
+        modelMap.addAttribute("message", message.getMessage());
+        return message.getMessage();
     }
 }
