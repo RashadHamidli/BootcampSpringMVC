c2c6da72b86b2eb20ca251fe85ea5034e91d5b30
Rashad Hamid <mr_rashad@email.com>
2023-10-27 14:14:35 +0400

    add

--- src/main/java/com/company/controller/TestController.java
+++ src/main/java/com/company/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.company.controller;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,10 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
         return "test printed";
+    }
+
+    @RequestMapping({"/page", "/{id}"})
+    public ResponseEntity<String> test2(@PathVariable(value = "id", required = false) long id) {
+        return ResponseEntity.ok("hello test");
     }
 }
