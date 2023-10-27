0fa7781cf43ec93fb3ece9d998d14cca054aa42d
Rashad Hamid <mr_rashad@email.com>
2023-10-27 14:25:56 +0400

    add

--- src/main/java/com/company/controller/TestController.java
+++ src/main/java/com/company/controller/TestController.java
@@ -15,8 +15,17 @@ public class TestController {
         return "test printed";
     }
 
-    @RequestMapping({"/page", "/{id}"})
-    public ResponseEntity<String> test2(@PathVariable(value = "id", required = false) long id) {
-        return ResponseEntity.ok("hello test");
+//    @RequestMapping("/{id}")
+//    public ResponseEntity<String> test1(@PathVariable(value = "id", required = false) long id) {
+//        return ResponseEntity.ok("hello test1");
+//    }
+//    @RequestMapping("/{id}")
+//    public ResponseEntity<String> test2(@PathVariable(required = false) long id) {
+//        return ResponseEntity.ok("hello test2");
+//    }
+
+    @RequestMapping("/{id}")
+    public ResponseEntity<String> test3(@PathVariable(value = "id", required = false) long l) {
+        return ResponseEntity.ok("hello test3");
     }
 }
