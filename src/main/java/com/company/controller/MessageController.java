334664a27aea4996de0c79cfb8233de28ed12926
Rashad Hamid <mr_rashad@email.com>
2023-10-27 14:51:41 +0400

    add

--- src/main/java/com/company/controller/MessageController.java
+++ src/main/java/com/company/controller/MessageController.java
@@ -3,23 +3,26 @@ package com.company.controller;
 import com.company.model.Message;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/app")
 public class MessageController {
-    @GetMapping("/message1")
+    @GetMapping({"/message1", "msg1", "mesaj1"})
     public String index() {
         Message message = new Message();
         message.setMessage("Hello Spring MVC! message 1");
         return message.getMessage();
     }
-    @GetMapping("/message2")
+
+    @RequestMapping(value = "/message2", name = "msg2", method = RequestMethod.GET)
     public String index2() {
         Message message = new Message();
         message.setMessage("Hello Spring MVC! message 2");
         return message.getMessage();
     }
+
     @GetMapping("/message3")
     public String index3() {
         Message message = new Message();
--- src/main/java/com/company/controller/TestController.java
+++ src/main/java/com/company/controller/TestController.java
@@ -2,30 +2,23 @@ package com.company.controller;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/app")
 public class TestController {
-    @GetMapping("/test")
-    public String test() {
-        return "test printed";
-    }
 
-//    @RequestMapping("/{id}")
-//    public ResponseEntity<String> test1(@PathVariable(value = "id", required = false) long id) {
-//        return ResponseEntity.ok("hello test1");
+    @RequestMapping(path = "/test")
+    public ResponseEntity<String> test1(@PathVariable(value = "id", required = false) long id) {
+        return ResponseEntity.ok("hello test1");
 //    }
 //    @RequestMapping("/{id}")
 //    public ResponseEntity<String> test2(@PathVariable(required = false) long id) {
 //        return ResponseEntity.ok("hello test2");
 //    }
 
-    @RequestMapping("/{id}")
-    public ResponseEntity<String> test3(@PathVariable(value = "id", required = false) long l) {
-        return ResponseEntity.ok("hello test3");
+//    @RequestMapping("/{id}")
+//    public ResponseEntity<String> test3(@PathVariable(value = "id", required = false) long l) {
+//        return ResponseEntity.ok("hello test3");
     }
 }
