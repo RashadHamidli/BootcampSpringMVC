0a4884dd2cd8138da4f8e70a4a24392711196332
Rashad Hamid <mr_rashad@email.com>
2023-10-27 19:38:00 +0400

    add

--- src/main/java/com/company/controller/MessageController.java
+++ src/main/java/com/company/controller/MessageController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/app")
 public class MessageController {
-    @GetMapping("/a{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<String> msg(@ModelAttribute(value = "id") Long id) {
+        return ResponseEntity.ok(String.valueOf(id));
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<String> message(@ModelAttribute(value = "id") Long id){
         return ResponseEntity.ok(String.valueOf(id));
     }
 }
