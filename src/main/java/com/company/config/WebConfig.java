5962d1ffccc9fade499a7962ae894f5498802ba4
Rashad Hamid <mr_rashad@email.com>
2023-10-27 15:51:37 +0400

    add

--- src/main/java/com/company/config/WebConfig.java
+++ src/main/java/com/company/config/WebConfig.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.util.UrlPathHelper;
 
 @ComponentScan
 @EnableWebMvc
 @ComponentScan(basePackages = "com.company")
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
     @Bean
     public ViewResolver viewResolver(){
         InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
@@ -17,4 +20,11 @@ public class WebConfig {
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setRemoveSemicolonContent(false);
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
+
 }
--- src/main/java/com/company/controller/TestController.java
+++ src/main/java/com/company/controller/TestController.java
@@ -1,15 +1,31 @@
 package com.company.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/app")
 public class TestController {
 
-    @RequestMapping(path = "/test")
+    @RequestMapping(path = "/test{id}")
     public ResponseEntity<String> test1(@RequestParam(value = "id", required = false, defaultValue = "1234") long id) {
         return ResponseEntity.ok(String.valueOf(id));
+    }
+
+    @GetMapping("/sayfa")
+    public ResponseEntity<?> sayfa(HttpServletRequest request, HttpSession httpSession) {
+        boolean esit = request.getSession().equals(httpSession);
+        return ResponseEntity.ok("Eşit" + (esit ? "tir" : "değildir."));
+    }
+    @GetMapping("/s")
+    public ResponseEntity<?> sayfa(HttpServletRequest request, InputStream inputStream)
+            throws Exception {
+        boolean esit = request.getInputStream().equals(inputStream);
+        return ResponseEntity.ok("Eşit" + (esit ? "tir" : "değildir."));
     }
 }
