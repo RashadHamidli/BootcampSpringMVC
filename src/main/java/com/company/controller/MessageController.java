361ee9827b9802bd4670103c8d20e6b772f5a13b
Rashad Hamid <mr_rashad@email.com>
2023-10-28 19:57:17 +0400

    add

--- src/main/java/com/company/controller/MessageController.java
+++ src/main/java/com/company/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.company.controller;
 
+import com.company.model.Message;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,13 +8,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/app")
 public class MessageController {
+    @GetMapping("/aaa")
+    @ResponseBody
+    public Message index() {
+        Message mesaj = new Message();
+        mesaj.setMessage("Merhaba Spring MVC!");
+        return mesaj;
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<String> msg(@ModelAttribute(value = "id") Long id) {
         return ResponseEntity.ok(String.valueOf(id));
     }
 
     @GetMapping("/id")
-    public ResponseEntity<String> message(@ModelAttribute(value = "id") Long id){
+    @ModelAttribute(value = "id")
+    public ResponseEntity<String> message(Long id){
         return ResponseEntity.ok(String.valueOf(id));
     }
 }
