894fa47e61f80f6755064c8aea8c444fc4508d4d
Rashad Hamid <mr_rashad@email.com>
2023-10-29 16:47:24 +0400

    add

--- src/main/java/com/company/dao/UserRepository.java
+++ src/main/java/com/company/dao/UserRepository.java
@@ -1,10 +1,18 @@
 package com.company.dao;
 
 import com.company.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Modifying
+    @Transactional
+    @Query("INSERT INTO User u (u.name, u.surname, u.email, u.password) VALUES (:name, :surname, :email, :password)")
+    void createUser(@Param("name") String name, @Param("surname") String surname, @Param("email") String email, @Param("password") String password);
 
 }
---
+++ src/main/java/com/company/services/UserService.java
@@ -0,0 +1,21 @@
+package com.company.services;
+
+import com.company.dao.UserRepository;
+import com.company.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+}
