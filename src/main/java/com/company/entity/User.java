56f0cf67c40dc19ad7c0f9ce7e07be8f2e668223
Rashad Hamid <mr_rashad@email.com>
2023-10-29 16:33:42 +0400

    add

---
+++ src/main/java/com/company/entity/User.java
@@ -0,0 +1,21 @@
+package com.company.entity;
+
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Value;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+}
