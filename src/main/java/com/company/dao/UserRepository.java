d2a53cd5f4721cd71bff36c75d49b060c65eefc3
mr_rashad <mr_rashad@email.com>
2023-10-31 23:03:43 +0400

    add

--- src/main/java/com/company/dao/UserRepository.java
+++ src/main/java/com/company/dao/UserRepository.java
@@ -2,12 +2,20 @@ package com.company.dao;
 
 import com.company.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
+
     User findByEmailAndPassword(String email, String password);
+
     void deleteUserByName(String name);
+
+    @Query(value = "select * from user where id=?1", nativeQuery = true)
+    User findByNameToId(Long id);
 }
