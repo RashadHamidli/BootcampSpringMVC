36318ee57bf343823cc7c6f88fdbe3e94f5d50f9
mr_rashad <mr_rashad@email.com>
2023-11-10 13:19:36 +0400

    add

--- src/test/java/com/truongbn/security/SecurityApplicationTests.java
+++ src/test/java/com/truongbn/security/SecurityApplicationTests.java
@@ -1,13 +1,13 @@
-package com.company;
+package com.truongbn.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LouncherTests {
+class SecurityApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
